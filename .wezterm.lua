3c3fa02e6f0a1dcce942f66fc598f0e5d48db27e
0xSmartCrypto <0xSmartCrypto@gmail.com>
2024-09-01 13:21:04 +0000

    aurora bg

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,15 +5,16 @@ local config = wezterm.config_builder()
 -- colorscheme
 config.color_scheme = "Tango (terminal.sexy)"
 config.background = {
-	{
-		source = {
-			-- File = '/Users/e/Documents/term-bg.png'
-			--File = "/Users/e/Documents/nebula.png",
-			--File = "/Users/e/Documents/milkyway1.png",
-			File = "/Users/e/Documents/milkyway2.png",
-		},
-		hsb = { brightness = 0.08 },
-	},
+  {
+    source = {
+      -- File = '/Users/e/Documents/term-bg.png'
+      --File = "/Users/e/Documents/nebula.png",
+      --File = "/Users/e/Documents/milkyway1.png",
+      --File = "/Users/e/Documents/milkyway2.png",
+      File = "/Users/e/Documents/aurora.jpg",
+    },
+    hsb = { brightness = 0.11 },
+  },
 }
 
 -- scrollback
@@ -25,10 +26,10 @@ config.scrollback_lines = 500000
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.window_padding = {
-	left = 8,
-	right = 8,
-	top = 20,
-	bottom = 20,
+  left = 8,
+  right = 8,
+  top = 20,
+  bottom = 20,
 }
 
 config.initial_cols = 200
